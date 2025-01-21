1f7dd34372073cfa67992d2c0f1d0e679a72f2d1
Isadora <imaciel2917@gmail.com>
2025-01-21 19:49:53 -0300

    feat: ação de adicionar to-do implementada

--- JS/to_do_list.js
+++ JS/to_do_list.js
@@ -1,10 +1,43 @@
-const list = document.querySelector('.list-task');
-const addTask = document.querySelector('.input-task');
-const buttonAddTask = document.querySelector('.button-add-task');
+const list = document.querySelector('.list-task'); //ul
+const inputTask = document.querySelector('.input-task'); //input
+const buttonAddTask = document.querySelector('.button-add-task'); //button
 
-function criarElem() {
+buttonAddTask.addEventListener("click", function() {
+    let novoLi = document.createElement("li")
+    novoLi.className = "task"
 
-}
+    let imgChecked = document.createElement("img")
+    let imgDelete = document.createElement("img")
+    let imgEditar = document.createElement("img")
+    let btChecked = document.createElement("button")
+    let btDelete = document.createElement("button")
+    let btEditar = document.createElement("button")
+
+    imgChecked.src = "../img/checked.png"
+    imgChecked.alt = "Concluído"
+    imgDelete.src = "../img/trash.png"
+    imgDelete.alt = "Deletar"
+    imgEditar.src = ""  // colocar o caminho da imagem do botão editar aqui
+    imgEditar.alt = "Editar"
+    btChecked.className = "btConcluido"
+    btDelete.className = "btDeletar"
+    btEditar.className = "btEditar"
+
+    btChecked.appendChild(imgChecked)
+    btDelete.appendChild(imgDelete)
+    btEditar.appendChild(imgEditar)
+
+    let nomeTarefa = document.createElement("p")
+    nomeTarefa.textContent = inputTask.value
+    inputTask.value = ''
+
+    novoLi.appendChild(btChecked)
+    novoLi.appendChild(nomeTarefa)
+    novoLi.appendChild(btEditar)
+    novoLi.appendChild(btDelete)
+
+    list.appendChild(novoLi)
+})
 
 function lerElem(){
 
